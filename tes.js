6053c1dc041c53be8d68cde16b32fb33f2e45e31
Sean Gondowardoyo <seangondo0@gmail.com>
2023-03-22 08:54:19 +0700

    update tes.js

--- tes.js
+++ tes.js
@@ -16,21 +16,22 @@ var count = 0;
 
 var woke = [];
 
-var totalSensor = 0;
+var sensorArr = [];
+
+var totalSensor = [];
 
 const sum = [];
 const val = [];
-const hslAvg = [];
+const avg = [];
 
 const trendType = [];
 const trendSum = [];
 const trend = [];
 const trendAvg = [];
 
-const db_username = [];
 let db_houseID = [];
 const userData = [];
-const robot_ID = [];
+const sensorData = [];
 
 var trendSend = '';
 
@@ -61,17 +62,13 @@ conn.connect((err)=>{
 //-------------------< MQTT Subscribe Topic on Connect >-------------------//
 client.on('connect', () => {
     var count = 0;
-    let sql = `SELECT * FROM client_table`;
+    let sql = `SELECT * FROM client_table ORDER BY house_id ASC`;
     let query = conn.query(sql, (err,result)=>{
         if (err) throw err;
         for(i = 0; i < result.length; i++){
-            db_username[i] = result[i]['username'];
-            
-            userData[`${db_username[i]}`] = result[i]['house_id'];
-            robot_ID[`${db_username[i]}`] = result[i]['robot_id'];
+            userData.push({'username': result[i]['username'], 'house_id': result[i]['house_id'], 'robot_id': result[i]['robot_id']});
             db_houseID = db_houseID.concat(result[i]['house_id']);
             if(db_houseID[count] == db_houseID[count-1]) {
-                console.log("MASOK");
                 const x = db_houseID.pop();
             }
             count++;
@@ -85,35 +82,66 @@ client.on('connect', () => {
         
         for(i = 0; i < db_houseID.length; i++) {
             client.subscribe(`${db_houseID[i]}/#`, function (err) {
-                // sum['10001'] = sum['10001'].concat(0)
-                // console.log(sum['10001'][7])
                 if(err) {
                     console.log('Failed to subscribe! Check Server!')
                 }
             })
+            totalSensor[db_houseID[i]] = 0;
         }
+        sensorList();
     });
+
 })
+
 
 
 //-------------------< MQTT Subscribe Topic on Connect >-------------------//
 client.on('message', function(topic, message) {
+    var split = splitTopic('/'+topic+'/', message);
+    checkDb(split);
 
-    splitTopic('/'+topic+'/', message);
-    // console.log("Msg : "+message);
+    split[2]
+    db_houseID;
 
+    for(i = 0; i < db_houseID.length; i++) {
+        if(split[0] == db_houseID[i]) {
+            for(j = 0; j < sensorData.length; j++) {
+                if(split[2] == sensorData[j]['room'] && split[3] == sensorData[j]['sensor_type']) {
+                    
+                }
+            }
+        }
+    }
+
+    // if(split[0] == sensorData[0]) {
+    //     console.log('Msg ' + message)
+    // }
+
+    // console.log(userData);
+    // console.log(sensorData);
+
+    // for(i = 0; i < db_houseID.length; i++) {
+    //     console.log(db_houseID[i])
+    // }
 });
 
+function sensorList(){
+    let sql = `SELECT * FROM db_sensor ORDER BY house_id ASC`;
+    let query = conn.query(sql, (err,result)=>{
+        if (err) throw err;
+        for(i = 0; i < result.length; i++){
+            sensorData.push({'house_id': result[i]['house_id'], 'room': result[i]['room'], 'sensor_type': result[i]['sensor_type']});
+        }
+    });
+}
 
 function splitTopic(topic, value) {
     var arrObj = []
     var i = 0;
     var index = 0;
-    // console.log('\n\nStarting Splice Num : ' + count);
     while (index != -1) { 
         var start = index + 1;
         index = topic.indexOf('/', index + 1);
-        // console.log(index);
         if(index != -1) {
             arrObj[i] = topic.substring(start, index)
             i++;
@@ -121,21 +149,20 @@ function splitTopic(topic, value) {
             break;
         }
     }
-    checkDb(arrObj);
-
+    return arrObj;
+    // console.log(woke.length);
 }
 
 function checkDb(data) {
     if(data[1] == 'receive_sensor') {
-        varCheck(data[0]);
         let sql = `SELECT COUNT(house_id) AS n FROM db_sensor WHERE house_id='${data[0]}' AND room='${data[2]}' AND sensor_type='${data[3]}'`;
         let query = conn.query(sql, (err,result)=>{
             if (err) throw err;
             if (result[0]['n'] == 0) {
                 addSensorDb(data);
-
             }
         });
+        varCheck(data[0]);
     }
 }
 
@@ -147,14 +174,22 @@ function addSensorDb(data) {
 }
 
 function varCheck(id) {
-    let sql = `SELECT COUNT(house_id) AS n FROM db_sensor WHERE house_id='${id}'`;
+    //let sql = `SELECT COUNT(house_id) AS n FROM db_sensor WHERE house_id='${id}'`;
+    let sql = `SELECT * FROM db_sensor WHERE house_id='${id}'`;
     let query = conn.query(sql, (err,result)=>{
         if (err) throw err;
-        if(totalSensor != result[0]['n']) {
-            totalSensor = result[0]['n'];
-            for(i = 0; i < totalSensor; i++){
-                woke = woke.concat(0);
+        // if(totalSensor != result[0]['n']) {
+        if(totalSensor[id] !== result.length) {
+            totalSensor[id] = result.length;
+            for(i = 0; i < totalSensor[id]; i++) {
+                sum.push({'house_id': id, 'room': result[i]['room'], 'type': result[i]['sensor_type'], 'val': 0});
+                val.push({'house_id': id, 'room': result[i]['room'], 'type': result[i]['sensor_type'], 'val': 0});
+                avg.push({'house_id': id, 'room': result[i]['room'], 'type': result[i]['sensor_type'], 'val': 0});
             }
         }
     });
+}
+
+function avgVal(total, count) { 
+
 }
