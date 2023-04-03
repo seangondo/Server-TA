ec42a05fbe030a87de70fa0fd3642a35f10ac027
seang <seangondo0@gmail.com>
2023-04-03 10:10:52 +0700

    ADD image to REST-server

--- REST-server.js
+++ REST-server.js
@@ -26,14 +26,29 @@ app.get('/',function(req,res){
 
 app.post('/apps/caregiver/login', (req,res) => {
     let data = {username: req.body.username, password:req.body.password};
-    var sql = `SELECT caregiver_id FROM caregiver_list WHERE (username="${req.body.username}" OR email="${req.body.username}") AND password="${req.body.password}"`;
+    var sql = `SELECT * FROM caregiver_list WHERE (username="${req.body.username}" OR email="${req.body.username}") AND password="${req.body.password}"`;
     var cmdResult = false;
+    
+    //Caregiver Details
+    var id = '';
+    var name = '';
+    var username = '';
+    var email = '';
+    var phone_number = '';
+    var address = '';
+
     var data_send = '';
     let query = conn.query(sql, data, (err, result)=>{
         if (err) throw err;
         console.log(result.length);
         if(result.length !== 0) {
             cmdResult = true;
+            id = result[0]['caregiver_id'];
+            name = result[0]['name'];
+            username = result[0]['username'];
+            email = result[0]['email'];
+            phone_number = result[0]['phone_number'];
+            address = result[0]['address'];
             var sql = `SELECT * FROM elder_list INNER JOIN elder_caregiver ON elder_list.elder_id = elder_caregiver.elder_id WHERE elder_caregiver.caregiver_id=${result[0]['caregiver_id']}`;
             let query = conn.query(sql, data, (err, result)=>{
                 if (err) throw err;
@@ -46,15 +61,16 @@ app.post('/apps/caregiver/login', (req,res) => {
                         "address": "${result[i]['address']}",
                         "house_id": "${result[i]['house_id']}",
                         "robot_id": "${result[i]['robot_id']}",
-                        "watch_id": "${result[i]['watch_id']}"
+                        "watch_id": "${result[i]['watch_id']}",
+                        "image": "${result[i]['image']}"
                     }`
                 }
                 data_send += `]`
-                res.send(`{"username": "${req.body.username}", "command": "login", "result": ${cmdResult}, ${data_send}}`);
+                res.send(`{"id": ${id}, "name": "${name}", "username": "${username}", "email": "${email}", "phone_number": "${phone_number}", "address": "${address}", "command": "login", "result": ${cmdResult}, ${data_send}}`);
             });
         } else {
             cmdResult = false;
-            res.send(`{"username": "${req.body.username}", "command": "login", "result": ${cmdResult}}`);
+            res.send(`{"id": null, "username": "${req.body.username}", "email": null, "phone_number": null, "command": "login", "result": ${cmdResult}}`);
         }
     })
 });
@@ -63,3 +79,5 @@ app.post('/apps/caregiver/login', (req,res) => {
  app.listen(8000,()=>{
     console.log('Server Running \nIP\t: localhost \nPort\t: 8000\n\n');
 });
+
+// TODO apps get sensors data etc
